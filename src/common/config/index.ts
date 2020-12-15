2b2324ddaec5844595494a3d2f0e6cefeb789abc
Wesley Matos <wrickee@gmail.com>
2020-12-15 11:28:05 -0300

    chore: fix env prop name

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -1,5 +1,5 @@
 export const { PORT = 3000 } = process.env;
 export const { NODE_ENV } = process.env;
 export const { GIPHY_API_KEY } = process.env;
-export const { RECIPE_PUPPY_URL } = process.env;
+export const { RECIPE_PUPPY_API_URL } = process.env;
 export const { GIPHY_API_URL } = process.env;
