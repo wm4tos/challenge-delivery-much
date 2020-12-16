56acaadaa7d65d1604fd6425089488cf68361962
Wesley Matos <wrickee@gmail.com>
2020-12-16 13:23:19 -0300

    chore: make error messages enum to giphy module

---
+++ src/modules/giphy/error.enum.ts
@@ -0,0 +1,7 @@
+export enum GiphyErrors {
+  NOT_ONLINE = 'Giphy API is not online',
+  BAD_REQUEST = 'You request is malformed',
+  FORBIDDEN = 'Your API key is incorrect',
+  NOT_FOUND = 'GIF not found',
+  TOO_MANY_REQUESTS = 'You made too many requests with this API key',
+}
