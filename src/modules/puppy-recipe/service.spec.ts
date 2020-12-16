027b0721ff4b371bc6a7ce0f6a290c11cc69e883
Wesley Matos <wrickee@gmail.com>
2020-12-16 01:33:53 -0300

    test: write puppy recipe tests

---
+++ src/modules/puppy-recipe/service.spec.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { getReasonPhrase } from 'http-status-codes';
+import { PuppyRecipeDto } from './interfaces/puppy-recipe.dto';
+import { getPuppyRecipes } from './service';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('PuppyRecipe service', () => {
+  it('should get recipe correctly', () => {
+    const expected = [{} as PuppyRecipeDto];
+
+    mockedAxios.get.mockResolvedValue({ data: { results: expected } });
+
+    expect(getPuppyRecipes([])).resolves.toStrictEqual(expected);
+  });
+
+  it('should receive bad request error', () => {
+    const status = 400;
+    const expected = { name: getReasonPhrase(status) };
+
+    mockedAxios.get.mockRejectedValue({ response: { status } });
+
+    expect(getPuppyRecipes([])).rejects.toMatchObject(expected);
+  });
+
+  it('should receive internal server error', () => {
+    const status = 500;
+    const expected = { name: getReasonPhrase(status) };
+
+    mockedAxios.get.mockRejectedValue({ response: { status } });
+
+    expect(getPuppyRecipes([])).rejects.toMatchObject(expected);
+  });
+});
