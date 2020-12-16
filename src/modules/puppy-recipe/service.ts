1ef6fa834e55ec6ea9028dc706cfc69523e16b34
Wesley Matos <wrickee@gmail.com>
2020-12-16 01:34:08 -0300

    feat: write puppy recipe service

---
+++ src/modules/puppy-recipe/service.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { getReasonPhrase } from 'http-status-codes';
+import { RECIPE_PUPPY_API_URL } from 'src/common/config';
+import { PuppyRecipeDto } from './interfaces/puppy-recipe.dto';
+
+export const getPuppyRecipes = async (ingredients: string[]): Promise<PuppyRecipeDto[]> => {
+  try {
+    const { data: { results } } = await axios.get(`${RECIPE_PUPPY_API_URL}/`, { params: { i: ingredients.join(',') } });
+
+    return results;
+  } catch (err) {
+    const { response: { status } } = err;
+
+    err.name = getReasonPhrase(status || 500);
+
+    throw err;
+  }
+};
