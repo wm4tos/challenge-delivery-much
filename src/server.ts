46eda7664eac79f90e02b0850ec7599b6cbbbe8d
Wesley Matos <wrickee@gmail.com>
2020-12-17 12:16:47 -0300

    chore: log data with logger helper

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import { getReasonPhrase } from 'http-status-codes';
 import routes from './routes';
 import { errorHandler } from './common/middlewares';
+import { logger } from './common/helpers';
 
 const app = express();
 
@@ -22,5 +23,5 @@ app.use((_, res, next) => next({ name: getReasonPhrase(404) }));
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  process.stdout.write(`Listening on port ${PORT}\n`);
+  logger.info(`Listening on port ${PORT}\n`);
 });
