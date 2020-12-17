5ac45ba36a0089a205174c97e83124d77509d550
Wesley Matos <wrickee@gmail.com>
2020-12-17 11:39:54 -0300

    refactor: use error handler to throw not found error

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,9 @@ import { PORT } from 'src/common/config';
 import express, { Router } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import { getReasonPhrase } from 'http-status-codes';
 import routes from './routes';
 import { errorHandler } from './common/middlewares';
-import { error } from './common/helpers';
 
 const app = express();
 
@@ -17,13 +17,9 @@ app.use(bodyParser.text());
 
 app.use('/api', routes(Router()));
 
+app.use((_, res, next) => next({ name: getReasonPhrase(404) }));
+
 app.use(errorHandler);
-
-app.use((_, res) => {
-  const err = error('NOT_FOUND');
-
-  return res.status(err.status).json(err);
-});
 
 app.listen(PORT, () => {
   process.stdout.write(`Listening on port ${PORT}\n`);
