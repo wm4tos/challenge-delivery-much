ed72204b37a062ce54f6f9bdcc90e6bc79c82c33
Wesley Matos <wrickee@gmail.com>
2020-12-17 12:17:26 -0300

    chore: log recipe data info

--- src/modules/recipes/controller.ts
+++ src/modules/recipes/controller.ts
@@ -1,7 +1,10 @@
 import { RequestHandler } from 'express';
+import { loggerData as logger } from 'src/common/helpers';
 import { getRecipes } from './service';
 
 export const getRecipesRoute: RequestHandler = async (req, res, next) => {
+  const log = { filename: __filename, type: req.method, endpoint: req.url };
+
   try {
     const { i } = req.query;
 
@@ -9,11 +12,17 @@ export const getRecipesRoute: RequestHandler = async (req, res, next) => {
 
     const recipes = await getRecipes(keywords);
 
-    return res.status(200).json({
+    const response = {
       keywords,
       recipes,
-    });
+    };
+
+    logger('info', { ...log, statusCode: 200, response });
+
+    return res.status(200).json(response);
   } catch (error) {
+    error.log = log;
+
     return next(error);
   }
 };
