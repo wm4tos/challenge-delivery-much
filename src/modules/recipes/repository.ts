4513f85b9eca86cd395ff9c2ad5fcd926032a9a4
Wesley Matos <wrickee@gmail.com>
2020-12-17 13:02:03 -0300

    feat: create recipes repository

---
+++ src/modules/recipes/repository.ts
@@ -0,0 +1,14 @@
+import { REDIS_EXPIRES } from 'src/common/config';
+import { client } from 'src/redis';
+
+export const save = (
+  key: string, value: unknown,
+): Promise<boolean> => client.setAsync(key, JSON.stringify(value), 'EX', Number.parseInt(REDIS_EXPIRES));
+
+export const get = async (key: string): Promise<any> => {
+  const data = await client.getAsync(key);
+
+  if (data) return JSON.parse(data);
+
+  return null;
+};
