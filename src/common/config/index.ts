4f7805502d356bc3dda9c1bfa025e709d7d47d2d
Wesley Matos <wrickee@gmail.com>
2020-12-17 13:01:47 -0300

    chore: export redis envs

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -3,3 +3,5 @@ export const { NODE_ENV } = process.env;
 export const { GIPHY_API_KEY } = process.env;
 export const { RECIPE_PUPPY_API_URL } = process.env;
 export const { GIPHY_API_URL } = process.env;
+export const { REDIS_URL } = process.env;
+export const { REDIS_EXPIRES } = process.env;
