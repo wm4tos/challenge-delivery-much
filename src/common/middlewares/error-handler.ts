aff221a5c574f335569c972143bae5de23217779
Wesley Matos <wrickee@gmail.com>
2020-12-17 12:17:07 -0300

    chore: log error on error handler middleware

--- src/common/middlewares/error-handler.ts
+++ src/common/middlewares/error-handler.ts
@@ -1,9 +1,15 @@
 import { ErrorRequestHandler } from 'express';
-import { error } from '../helpers';
+import { error, logger } from '../helpers';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const errorHandler: ErrorRequestHandler = (_err, req, res, next) => {
   const err = error(_err.name, _err.message);
+
+  const { log = {} } = _err;
+
+  logger.error(JSON.stringify({
+    ...log, endpoint: req.url, response: err, statusCode: err.status,
+  }));
 
   return res.status(err.status).json(err);
 };
