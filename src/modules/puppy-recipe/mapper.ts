c3be373af9668eba2ef9096af357987197b7073d
Wesley Matos <wrickee@gmail.com>
2020-12-21 14:53:52 -0300

    feat: create mapper to sort ingredients

--- src/modules/puppy-recipe/mapper.ts
+++ src/modules/puppy-recipe/mapper.ts
@@ -1,4 +1,4 @@
-import { omit } from 'ramda';
+import { omit, trim } from 'ramda';
 import { renameKeys } from 'ramda-adjunct';
 import { MappedPuppyRecipeDto } from './interfaces/mapped-puppy-recipe.dto';
 import { PuppyRecipeDto } from './interfaces/puppy-recipe.dto';
@@ -17,4 +17,14 @@ export const changeKeys: ChangeKeys = (recipe) => {
   const keyMap = { href: 'link' };
 
   return renameKeys(keyMap)(recipe) as MappedPuppyRecipeDto;
+};
+
+interface SortIngredients {
+  (recipe: PuppyRecipeDto): PuppyRecipeDto
+}
+
+export const sortIngredients: SortIngredients = ({ ingredients, ...recipe }) => {
+  const i = (ingredients as string).split(',').map(trim).sort();
+
+  return { ...recipe, ingredients: i } as PuppyRecipeDto;
 };
