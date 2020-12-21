8911da19159e70c4de8516a2fc958e80b9b60870
Wesley Matos <wrickee@gmail.com>
2020-12-21 14:54:31 -0300

    feat: add sortIngredients to mapRecipes compose

--- src/modules/recipes/mapper.ts
+++ src/modules/recipes/mapper.ts
@@ -5,14 +5,15 @@ import { GiphyDto } from '../giphy/interfaces/giphy.dto';
 import { getBetterGiphy, getGiphyUrl } from '../giphy/mapper';
 import { MappedPuppyRecipeDto } from '../puppy-recipe/interfaces/mapped-puppy-recipe.dto';
 import { PuppyRecipeDto } from '../puppy-recipe/interfaces/puppy-recipe.dto';
-import { changeKeys, removeThumbnail } from '../puppy-recipe/mapper';
+import { changeKeys, removeThumbnail, sortIngredients } from '../puppy-recipe/mapper';
 
 export const mapRecipes = compose<
-PuppyRecipeDto[], PuppyRecipeDto[], MappedPuppyRecipeDto[], MappedPuppyRecipeDto[] | Promise<MappedPuppyRecipeDto[]>
+PuppyRecipeDto[], PuppyRecipeDto[], PuppyRecipeDto[], MappedPuppyRecipeDto[], MappedPuppyRecipeDto[] | Promise<MappedPuppyRecipeDto[]>
 >(
   sortBy(prop('title')),
   map(changeKeys),
   map(removeThumbnail),
+  map(sortIngredients),
 );
 
 export const mapGiphyUrl = compose<GiphyDto[], GiphyDto, string>(getGiphyUrl, getBetterGiphy);
