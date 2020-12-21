f2b4522678cdddc0940b7bbbbeae9412a66c1b91
Wesley Matos <wrickee@gmail.com>
2020-12-21 14:54:07 -0300

    test: ingredients sorter

--- src/modules/puppy-recipe/mapper.spec.ts
+++ src/modules/puppy-recipe/mapper.spec.ts
@@ -1,7 +1,7 @@
 import { omit } from 'ramda';
 import { MappedPuppyRecipeDto } from './interfaces/mapped-puppy-recipe.dto';
 import { PuppyRecipeDto } from './interfaces/puppy-recipe.dto';
-import { changeKeys, removeThumbnail } from './mapper';
+import { changeKeys, removeThumbnail, sortIngredients } from './mapper';
 
 describe('PuppyRecipe mapper', () => {
   it('should remove thumbnail prop from object', () => {
@@ -24,5 +24,13 @@ describe('PuppyRecipe mapper', () => {
     const expected = { link: 'bla' } as any;
 
     expect(changeKeys(expected)).toStrictEqual(expected);
+  });
+
+  it('should return ingredients sorted alphabetically', () => {
+    const obj = { ingredients: 'onion,tomato,fries' } as any;
+
+    const expected = { ingredients: obj.ingredients.split(',').map((s) => s.trim()).sort() } as any;
+
+    expect(sortIngredients(obj)).toStrictEqual(expected);
   });
 });
